189e9a22534c93120b4036b687c5b9ca06f57e42
y-kanda <y_kanda@kyokko.co.jp>
2020-06-29 13:20:34 +0900

    Change the CSS for the site.

--- app/views.py
+++ app/views.py
@@ -6,3 +6,9 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+# def index(request):
+#     d = {
+#         'posts': Post.objects.all(),
+#     }
+#     return render(request, 'blog/index.html', d)
